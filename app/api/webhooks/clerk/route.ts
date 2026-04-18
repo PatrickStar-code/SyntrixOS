33a1c0839e48bbec279921149332e1f07d4f1972
PatrickStar <patrick.almeida06@gmail.com>
2026-04-18 16:40:31 -0300

    feat: created first webhook in clerk

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,23 +1,51 @@
-import { NextRequest, NextResponse } from "next/server";
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@/lib/supabase/client";
+import { NextResponse } from "next/server";
+import { Webhook } from "svix";
+import { WebhookEvent } from "@clerk/backend";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
-);
+const supabase = createClient();
 
-export async function POST(req: NextRequest) {
-  const body = await req.json();
+const secret = process.env.CLERK_WEBHOOK_SECRET;
 
-  if (body.type === "user.created") {
-    const user = body.data;
+if (!secret) {
+  throw new Error("CLERK_WEBHOOK_SECRET não definido");
+}
 
-    await supabase.from("accounts").insert({
-      user_id: user.id,
-      name: `${user.first_name} ${user.last_name}`,
-      created_at: new Date(),
-      updated_at: new Date(),
-    });
+const wh = new Webhook(secret);
+
+export async function POST(req: Request) {
+  const payload = await req.text();
+  const headers = req.headers;
+
+  let event: WebhookEvent;
+
+  try {
+    event = wh.verify(payload, {
+      "svix-id": headers.get("svix-id")!,
+      "svix-timestamp": headers.get("svix-timestamp")!,
+      "svix-signature": headers.get("svix-signature")!,
+    }) as WebhookEvent;
+  } catch {
+    return new Response("Invalid signature", { status: 400 });
+  }
+
+  console.log("EVENT TYPE:", event.type);
+
+  if (event.type === "user.created") {
+    const user = event.data;
+
+    const name = `${user.first_name || ""} ${user.last_name || ""}`.trim();
+    const now = new Date().toISOString();
+
+    await supabase.from("accounts").upsert(
+      {
+        user_id: user.id,
+        name,
+        created_at: now,
+        updated_at: now,
+      },
+      { onConflict: "user_id" },
+    );
   }
 
   return NextResponse.json({ success: true });
